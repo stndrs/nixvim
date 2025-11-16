5e181a0ace0ad9581c6e9b5ccb1f11b18809d6f8
stndrs <steph@stndrs.ca>
2025-11-16 00:12:38 -0700

    remove linter

--- config/plugins/lang/json.nix
+++ config/plugins/lang/json.nix
@@ -13,18 +13,6 @@
       };
     };
 
-    lint = {
-      lintersByFt = {
-        json = [ "jsonlint" ];
-      };
-
-      linters = {
-        jsonlint = {
-          cmd = "${pkgs.nodePackages_latest.jsonlint}/bin/jsonlint";
-        };
-      };
-    };
-
     lsp.servers.jsonls = {
       enable = true;
     };
